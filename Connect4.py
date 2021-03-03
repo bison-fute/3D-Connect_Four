7257ce70c9efe3640242492a47a695c4c0fd5935
Geoffrey Saunois <59168447+GeoffreySaunois@users.noreply.github.com>
2021-03-04 00:48:52 +0100

    Add files via upload

--- Connect4.py
+++ Connect4.py
@@ -1,10 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as npr
+import torch
 
-from keras.layers import Dense, Dropout, Conv2D, Input, Lambda, MaxPooling2D, UpSampling2D
-from keras import Model
 
+def seize_to_pos(n):
+    """
+    Transforme un nombre entre 0 et 15 en une position valable
+    pour Game.add_tokens
+    :param n:
+    :return:
+    """
+    return 10*(n//4) + n % 4
+
+def pos_to_seize(pos):
+    return 4*(pos//10) + (pos % 10)
 
 
 class Game:
@@ -15,6 +25,10 @@ class Game:
     def __init__(self):
         # Réprésentation naturelle du plateau de jeu -> 0: case vide, 1: jeton joueur 1, -1: jeton joueur 2
         self.board = np.zeros((4, 4, 4))
+
+        # États utilisés par les réseaux de neuronnes
+        self.board_j1 = torch.zeros(64)
+        self.board_j2 = torch.zeros(64)
 
         # Réprésentation binaire des positions des jetons du premier joueur
         # (ça permet de vérifier rapidement s'il y a un puissance 4)
@@ -36,7 +50,7 @@ class Game:
         # (une position devient non disponible une fois que la colonne correspondante est remplie)
 
 
-    def display_board(self):
+    def display_board(self, title=None):
         """
             Fonction utilitaire pour afficher le plateau de jeu
         """
@@ -55,10 +69,43 @@ class Game:
                         ax.scatter(i, j, k, c='yellow', s=100)
                     if self.board[i, j, k] == -1:
                         ax.scatter(i, j, k, c='red', s=100)
+        if title is not None:
+            plt.title(title)
         plt.show()
         return None
 
     def __add_token(self, pos):
+        """
+            Cf self.add_token
+        """
+        _x, _y = pos // 10, pos % 10
+        if self.ntok % 2 == 0:
+            _player = -1
+        else:
+            _player = 1
+        _z = 0
+        while self.board[_x, _y, _z] != 0:
+            _z += 1
+            if _z == 4:
+                break
+        if _z < 4:          # Ajout du jeton
+            self.board[_x, _y, _z] = _player
+            if _player == 1:
+                self.binboard_j1 += 2**(_x + 5*_y + 25*_z)
+                self.binboard_j1 = int(self.binboard_j1)
+                self.board_j1[_x + 4*_y + 16*_z] = 1
+            else:
+                self.binboard_j2 += 2**(_x + 5*_y + 25*_z)
+                self.binboard_j2 = int(self.binboard_j2)
+                self.board_j2[_x + 4 * _y + 16 * _z] = 1
+            self.ntok += 1
+            if _z == 3:
+                self.free_positions.remove(pos)
+        else:
+            raise ValueError('Colonne remplie')
+        return None
+
+    def remove_token(self, pos):
         """
             Cf self.add_token
         """
@@ -73,17 +120,22 @@ class Game:
             _z += 1
             if _z == 4:
                 break
-        if _z < 4:          # Ajout du jeton
-            self.board[_x, _y, _z] = _player
+        _z -= 1
+        if _z == -1:
+            raise ValueError('Colonne Vide')
+        else:  # Ajout du jeton
+            self.board[_x, _y, _z] = 0
             if _player == 1:
-                self.binboard_j1 += 2**(_x + 5*_y + 25*_z)
+                self.binboard_j1 -= 2 ** (_x + 5 * _y + 25 * _z)
+                self.binboard_j1 = int(self.binboard_j1)
+                self.board_j1[_x + 4*_y + 16*_z] = 0
             else:
-                self.binboard_j2 += 2**(_x + 5*_y + 25*_z)
-            self.ntok += 1
+                self.binboard_j2 -= 2 ** (_x + 5 * _y + 25 * _z)
+                self.binboard_j2 = int(self.binboard_j2)
+                self.board_j2[_x + 4 * _y + 16 * _z] = 0
+            self.ntok -= 1
             if _z == 3:
-                self.free_positions.remove(pos)
-        else:
-            raise ValueError('Colonne remplie')
+                self.free_positions.append(pos)
         return None
 
 
@@ -108,6 +160,9 @@ class Game:
     def get_ntokens(self):
         return self.ntok
 
+    def get_turn(self):
+        return self.ntok % 2
+
     def end_game(self):
         """
         Détermine si le plateau est rempli
@@ -121,6 +176,8 @@ class Game:
         :return: None
         """
         self.board = np.zeros((4, 4, 4))
+        self.board_j1 = torch.zeros(64)
+        self.board_j2 = torch.zeros(64)
         self.binboard_j1 = 0
         self.binboard_j2 = 0
         self.ntok = 0
@@ -128,6 +185,10 @@ class Game:
         for i in range(16):
             pos = (i//4) * 10 + (i % 4)
             self.free_positions.append(pos)
+
+    def reset(self):
+        self.clear_board()
+        return self.get_state()
 
     def check_connect4(self, show=True):
         """
@@ -142,6 +203,15 @@ class Game:
             print('Joueur 2 :', bool((bb2 & (bb2 >> self.deltas2)).any()))
         else:
             return (bb1 & (bb1 >> self.deltas2)).any() | (bb2 & (bb2 >> self.deltas2)).any()
+
+    def get_state(self):
+        if self.ntok % 2 == 0:
+            return torch.cat([self.board_j1, self.board_j2])
+        else:
+            return torch.cat([self.board_j2, self.board_j1])
+
+    def check_winning_move(self):
+        raise ValueError('To implement')
 
 
 ## Random player
